c4a36b8d61c373fa8938e62556c0a5dec5620704
Elizaveta Konovkina <70512444+ElizavetaKkk@users.noreply.github.com>
2023-06-13 01:51:36 +0300

    Several elements were added to the AddTodoItemFragment

--- app/src/main/java/com/example/todoapp/AddTodoItemFragment.kt
+++ app/src/main/java/com/example/todoapp/AddTodoItemFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class AddTodoItemFragment : Fragment() {
 
@@ -13,5 +15,16 @@ class AddTodoItemFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_add_todo_item, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val spinner: Spinner = view.findViewById(R.id.importance_list)
+        val importanceList = requireContext().resources.getStringArray(R.array.list_importance).toList()
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, importanceList)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+        spinner.setSelection(0)
     }
 }
