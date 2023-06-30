db28699196be5ab7e79edbe012fd9a8ba1d89630
Elizaveta Konovkina <70512444+ElizavetaKkk@users.noreply.github.com>
2023-06-30 22:51:50 +0300

    Final changes 2

--- app/src/main/java/com/example/todoapp/ui/todolist/recyclerview/TodoItemViewHolder.kt
+++ app/src/main/java/com/example/todoapp/ui/todolist/recyclerview/TodoItemViewHolder.kt
@@ -4,16 +4,23 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.todoapp.databinding.TodoitemPreviewBinding
 import com.example.todoapp.data.item.TodoItem
 import com.example.todoapp.ui.todolist.actions.TodoListUiAction
+import com.example.todoapp.utils.formatDate
 
 class TodoItemViewHolder(binding: TodoitemPreviewBinding) : RecyclerView.ViewHolder(binding.root) {
     private lateinit var todoItem: TodoItem
     private val checkBox = binding.checkBox
     private val todoText = binding.todoText
+    private val deadline = binding.deadlineText
 
     fun onBind(todoItem: TodoItem, onUiAction: (TodoListUiAction) -> Unit) {
         this.todoItem = todoItem
         checkBox.isChecked = todoItem.isDone
         todoText.text = todoItem.text
+
+        val deadlineDate = todoItem.deadline
+        if (deadlineDate != null) {
+            deadline.text = formatDate(deadlineDate)
+        }
 
         setupDiffCallback(onUiAction)
     }
