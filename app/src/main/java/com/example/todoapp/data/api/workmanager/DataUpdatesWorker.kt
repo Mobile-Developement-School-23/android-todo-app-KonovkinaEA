cdb27bfa4708971d352b7c5a1e1743ccc951c4bd
Elizaveta Konovkina <70512444+ElizavetaKkk@users.noreply.github.com>
2023-06-30 23:50:11 +0300

    Final changes 3

--- app/src/main/java/com/example/todoapp/data/api/workmanager/DataUpdatesWorker.kt
+++ app/src/main/java/com/example/todoapp/data/api/workmanager/DataUpdatesWorker.kt
@@ -3,13 +3,13 @@ package com.example.todoapp.data.api.workmanager
 import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import com.example.todoapp.data.api.Common
+import com.example.todoapp.Dependencies
 
 class DataUpdatesWorker(appContext: Context, workerParams: WorkerParameters) :
     CoroutineWorker(appContext, workerParams) {
 
     override suspend fun doWork(): Result {
-        val result = Common.apiService.getAllTodoData()
+        Dependencies.repository.loadDataFromServer()
         return Result.success()
     }
 }
